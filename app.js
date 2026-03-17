f53f4625f138165b9dc7c034f01b4e909a93a652
John <john.26sfj@gmail.com>
2026-03-17 10:32:59 +0100

    Added personalia page

---
+++ app.js
@@ -0,0 +1,5 @@
+const personaliaBtn = document.getElementById("personalia-btn");
+
+personaliaBtn.addEventListener("click", () => {
+  window.open(`./components/personalia.html`, `_blank`).focus();
+});
